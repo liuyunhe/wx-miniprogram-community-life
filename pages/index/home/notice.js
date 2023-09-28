5f4c637e9752143c2a60baaee23a0ca8bd37f1a0
liuyunhe <liuyunhe666@aliyun.com>
2023-09-28 10:29:37 +0800

    通知增加头图

--- pages/index/home/notice.js
+++ pages/index/home/notice.js
@@ -1,13 +1,13 @@
 const $api = require('../../../utils/api.js').API;
 Page({
-
   /**
    * 页面的初始数据
    */
   data: {
-    noticeId: '',
-    details:{},
-    noticeContent:''
+    noticeImage: "",
+    noticeId: "",
+    details: {},
+    noticeContent: ""
   },
 
   /**
@@ -20,15 +20,19 @@ Page({
   },
 
   notice() {
-    let _this = this;
+    let _this = this
     var noticeId = {
-      id: _this.data.noticeId,
-    };
-    $api.noticeDetail(noticeId).then(res => {
-      _this.data.noticeContent=res.value.noticeContent.replace(/\<img/gi, '<img style="max-width:100%;height:auto" ')
+      id: _this.data.noticeId
+    }
+    $api.noticeDetail(noticeId).then((res) => {
+      _this.data.noticeContent = res.value.noticeContent.replace(
+        /\<img/gi,
+        '<img style="max-width:100%;height:auto" '
+      )
       _this.setData({
+        noticeImage: res.value.noticeImage ? JSON.parse(res.value.noticeImage)[0].url:'',
         details: res.value,
-        noticeContent:_this.data.noticeContent
+        noticeContent: _this.data.noticeContent
       })
     })
   },
@@ -36,49 +40,37 @@ Page({
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
-  onReady() {
-
-  },
+  onReady() {},
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow() {
-    this.notice();
+    this.notice()
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
-  onHide() {
-
-  },
+  onHide() {},
 
   /**
    * 生命周期函数--监听页面卸载
    */
-  onUnload() {
-
-  },
+  onUnload() {},
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
-  onPullDownRefresh() {
-
-  },
+  onPullDownRefresh() {},
 
   /**
    * 页面上拉触底事件的处理函数
    */
-  onReachBottom() {
-
-  },
+  onReachBottom() {},
 
   /**
    * 用户点击右上角分享
    */
-  onShareAppMessage() {
-
-  }
+  onShareAppMessage() {}
 })
